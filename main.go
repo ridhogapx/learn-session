e570bf6f69b414b29a9986bfdef30c34450e0fb5
RageNeko26 <rneko2006@gmail.com>
2023-06-10 17:26:37 +0700

    feat: How to using Struct

--- main.go
+++ main.go
@@ -5,7 +5,36 @@ import (
 	"learning_go/helper"
 )
 
+type Student struct {
+	Name string
+	Age int
+	Address string
+	IsStudent bool
+}
+
 func main() {
+	var border string = "--------------------------"
+
 	result := helper.Sum(15,20)
 	fmt.Println(result)
+	fmt.Println(border)
+
+	var foo Student 
+	foo.Name = "Ridho"
+	foo.Age = 17
+	foo.Address = "Sukoharjo"
+	foo.IsStudent = true
+
+	fmt.Println(foo.Name)
+	fmt.Println(border)
+
+	bar := Student{
+		Name : "Joko",
+		Age : 28,
+		Address: "Sukoharjo",
+		IsStudent: false,
+	}
+
+	fmt.Println(bar)
+
 }
