f05396b722b5ddfc2554703839647b43a46ee3e2
RageNeko26 <rneko2006@gmail.com>
2023-06-11 22:07:19 +0700

    feat: Unused package

--- database/database.go
+++ database/database.go
@@ -1,9 +1,11 @@
 package database
 
+import ("fmt")
 var connection string
 
 func init() {
 	connection = "Mysql"
+	fmt.Println("Init is executed!")
 }
 
 func GetDatabase() string {
---
+++ package_initialization.go
@@ -0,0 +1,8 @@
+package main
+
+import _"learning_go/database"
+
+func main() {
+	// result := database.GetDatabase()
+	// fmt.Println(result)
+}
